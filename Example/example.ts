5b8e5892548bbed6dad27f8ee444dd8c850e605b
Vinicius Silva <viniciusvinisilva1@outlook.com>
2022-09-09 20:55:38 -0300

    fix

--- Example/example.ts
+++ Example/example.ts
@@ -1,4 +1,4 @@
-import { Maker } from '..';
+import { Maker } from '../src';
 
 new Maker().Ephoto360('https://en.ephoto360.com/create-colorful-angel-wing-avatars-731.html', ["Bruno Mars"]).then(res => {
     console.log(res)
